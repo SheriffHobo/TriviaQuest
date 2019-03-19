e7b41fcde3bf8f92edf1884a979ec34d6de121df
SheriffHobo <zombiemaster@gmail.com>
2019-03-18 18:55:29 -0700

    more ui changes

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,33 +1,31 @@
-$(function() {
+$(function () {
 
-    $(".title-letter").hover(function(){
+    $(".title-letter").hover(function () {
         var r = Math.floor(Math.random() * 255);
         var g = Math.floor(Math.random() * 255);
         var b = Math.floor(Math.random() * 255);
-        var color = "rgb("+r+","+g+","+b+")"
+        var color = "rgb(" + r + "," + g + "," + b + ")"
         $(this).css("color", color);
     });
 
 });
 var rootRef = firebase.database().ref();
 var urlRef = rootRef.child("users");
-urlRef.on("value", function(snapshot) {
-  $("#usersScoreBoard").html("");
-  snapshot.forEach(function(child) {
-    var userInfo = child.val();
+urlRef.on("value", function (snapshot) {
+    $("#usersScoreBoard").html("");
+    snapshot.forEach(function (child) {
+        var userInfo = child.val();
 
-    var displayName = userInfo.displayName;
-    var profileImg = userInfo.profile_picture;
-    var userPoints = userInfo.userPoints;
-    $("#usersScoreBoard").append(`
-    <div class="row ml-1 float-left">
-        <div class="col-2 text-center mt-3 mb-1"><img src='${profileImg}' class="rounded-circle playerScores"/></div>
-        <div class="col-6 text-center mt-3 mb-1">${displayName}</div>
-        <div class="col-4 text-center mt-3 mb-1">${userPoints}</div>
-    </div>
-    `);
-  });
+        var displayName = userInfo.displayName;
+        var profileImg = userInfo.profile_picture;
+        var userPoints = userInfo.userPoints;
+        $("#usersScoreBoard").append(`
+    <div class="row ml-1 float-left align-items-center text-center">
+    <div class="col text-center border-right border-white"><img src='${profileImg}' class="rounded-circle playerScores mr-1"/>${displayName}<br>Score: ${userPoints}</div>
+    </div>`
+        );
+    });
 });
 
 
-  
+
